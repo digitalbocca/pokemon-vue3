edc6269ef5aff39dfdd4b90b5970f79a370afef0
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-12 19:19:02 -0300

    Implementa cards

--- src/app.vue
+++ src/app.vue
@@ -5,14 +5,47 @@
   .content
     .search
       p Search
-    .cards
-      p Cards
+    .cards-container
+      card(v-for="pokemon, id in pokemons" :key="id")
+        template(#image)
+          img(:src="pokemon.image")
+        template(#title)
+          p {{ pokemon.name }}
+        template(#description)
+          p {{ pokemon.type }}
   
 </template>
 
 <script setup>
 
-import TopBar from './components/top-bar.vue'
+import TopBar from '@/components/top-bar.vue'
+import Card from '@/components/card.vue'
+
+const pokemons = [
+  { name: 'bulbasaur', type: 'grass', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' },
+  { name: 'ivysaur', type: 'grass', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png' },
+  { name: 'venusaur', type: 'grass', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png' },
+  { name: 'charmander', type: 'fire', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png' },
+  { name: 'charmeleon', type: 'fire', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png' },
+  { name: 'charizard', type: 'fire', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png' },
+  { name: 'squirtle', type: 'water', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png' },
+  { name: 'wartortle', type: 'water', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png' },
+  { name: 'blastoise', type: 'water', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png' },
+  { name: 'caterpie', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png' },
+  { name: 'metapod', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png' },
+  { name: 'butterfree', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png' },
+  { name: 'weedle', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png' },
+  { name: 'kakuna', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png' },
+  { name: 'beedrill', type: 'bug', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png' },
+  { name: 'pidgey', type: 'flying', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png' },
+  { name: 'pidgeotto', type: 'flying', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png' },
+  { name: 'pidgeot', type: 'flying', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png' },
+  { name: 'rattata', type: 'normal', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png' },
+  { name: 'raticate', type: 'normal', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png' },
+  { name: 'spearow', type: 'flying', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png' },
+  { name: 'fearow', type: 'flying', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png' },
+  { name: 'ekans', type: 'poison', image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png' },
+]
 
 </script>
 
@@ -20,5 +53,14 @@ import TopBar from './components/top-bar.vue'
 
 .home
   @apply flex flex-col items-center justify-center
+
+  .content
+    @apply flex flex-col items-center justify-center
+    @apply p-4 m-4
+    @apply w-full
+
+  .cards-container
+    @apply flex flex-wrap justify-center
+    @apply w-full
 
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+
+.card
+  slot(name="image")
+  slot(name="title")
+  slot(name="description")
+
+</template>
+
+<script>
+
+</script>
+
+<style lang="sass">
+
+.card
+  @apply flex flex-col items-center justify-center
+  @apply p-4 m-4
+  @apply bg-white
+  @apply rounded-lg
+  @apply drop-shadow
+
+</style>
