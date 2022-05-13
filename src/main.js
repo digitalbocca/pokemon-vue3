3f24f153f026be5b7bca9a604c70635e48927d6b
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2022-05-13 15:50:34 -0300

    Extrai home para uma view e configura router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import router from './router'
 
 import App from './app.vue'
 
@@ -8,5 +9,6 @@ import './sass/styles.sass'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
---
+++ src/views/home.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+
+.home
+  .search
+    input.search(type="text", placeholder="Pesquisar por Nome ou Codigo", v-model="store.search")
+  .cards-container
+    .cards
+      card(v-for="pokemon, id in filteredPokemons" :key="id")
+        template.image(#image)
+          img.poke-avatar(:src="pokemon.img")
+        template(#title)
+          p {{ pokemon.name }}
+        template(#description)
+          p Cód: {{ pokemon.id }}
+      load-more(v-if="loadMoreIsVisible && isLoading")
+      load-more(v-if="loadMoreIsVisible" @click="loadMore()")
+  
+</template>
+
+<script setup>
+
+import { onMounted, computed } from 'vue'
+
+import TopBar from '@/components/top-bar.vue'
+import Card from '@/components/card.vue'
+import LoadMore from '@/components/load-more.vue'
+
+import { GENERAL_LIMIT } from '@/constants'
+import { useStore } from '@/store/pokemons'
+
+const store = useStore()
+
+const pokemons = computed(() => store.getPokemons)
+const isLoading = computed(() => store.loading)
+const filteredPokemons = computed(() => store.filteredPokemons)
+
+const loadMoreIsVisible = computed(() => pokemons.value.length < GENERAL_LIMIT)
+
+const loadMore = store.fetchPokemons
+
+onMounted(() => {
+  store.fetchPokemons()
+})
+
+</script>
+
+<style lang="sass">
+
+.home
+  @apply flex flex-col items-center justify-center
+  @apply p-4 m-4
+  @apply w-full
+
+  .cards-container
+    @apply flex justify-center
+    @apply w-full
+
+    .cards
+      @apply flex flex-wrap content-start
+      @apply w-full
+
+      max-width: 850px
+  
+  .search
+    @apply flex flex-row items-center justify-center
+    @apply w-full
+    @apply p-4 m-4
+
+    max-width: 850px
+
+    input
+      @apply w-full
+      @apply py-2 px-4 m-2
+      @apply rounded-full
+      @apply drop-shadow
+
+</style>
